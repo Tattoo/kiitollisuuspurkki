0a033faca5dae14bd0782dfa725d3bf04324f3c4
Tatu Kairi <tatu.kairi@eficode.com>
2013-03-19 23:51:08 +0200

    trying out mongo still

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
-var express = require( "express" ),
-    mongo = require( "mongodb" );
-
-
-
+var express = require( "express" );
+    
 var app = express.createServer(express.logger());
-var mongo_uri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost/db"
+var db_uri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "kiitollisuuspurkki";
+var db = require( "mongojs" ).connect( db_uri, [ "thanks" ] );
 
 app.get("/", function(request, response) {
-  mongo.Db.connect( mongo_uri, function( err, db ){
-    if ( err ) {
-      res.send( "Can't connect to db" );
-      return false;
-    }
-    
-    var c = db.thanks.find();
-    var s = "";
-    while ( c.hasNext() ){
-      s += printjson( c.next() );
-      
-    }
-    
-    res.send( s );
-  });
+  response.send(db.thanks.find())
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
