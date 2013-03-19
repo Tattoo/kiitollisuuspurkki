a2b96c6c509668130c98791723de9a9d9c239b2c
Tatu Kairi <tatu.kairi@eficode.com>
2013-03-20 01:11:00 +0200

    added jade, cleaned up

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-var express = require( "express" );
+var express = require( "express" ),
+    mongojs = require( "mongojs" );
     
-var app = express.createServer(express.logger());
-var db_uri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "kiitollisuuspurkki";
-var db = require( "mongojs" ).connect( db_uri, [ "thanks" ] );
+var app = express.createServer( express.logger() ),
+    db_uri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "kiitollisuuspurkki",
+    db = mongojs.connect( db_uri, [ "thanks" ] );
 
-app.get("/", function(request, response) {
-  response.send(db.thanks.find())
+app.set( "views", __dirname + "/public" );
+app.set( "view options", { "layout": false} );
+
+app.use(express.static( __dirname + '/public' ));
+
+app.get( "/", function(req, res) {
+  res.render( "index.jade" );
+});
+
+app.post( "/create", function( req, res){
+  res.send( 404 );
 });
 
 var port = process.env.PORT || 8000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen( port, function() {
+  console.log( "Listening on " + port );
 });
