1aa0255c252353020af95efdb704d52669214120
Tatu Kairi <tatu.kairi@eficode.com>
2013-03-19 23:05:08 +0200

    playing around with mongo

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require( "express" ),
+    mongo = require( "mongodb" );
+
+
 
 var app = express.createServer(express.logger());
+var mongo_uri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost/db"
 
-app.get('/', function(request, response) {
-  
+app.get("/", function(request, response) {
+  mongo.Db.connect( mongo_uri, function( err, db ){
+    if ( err ) {
+      res.send( "Can't connect to db" );
+      return false;
+    }
+    
+    var c = db.thanks.find();
+    var s = "";
+    while ( c.hasNext() ){
+      s += printjson( c.next() );
+      
+    }
+    
+    res.send( s );
+  });
 });
 
 var port = process.env.PORT || 5000;
