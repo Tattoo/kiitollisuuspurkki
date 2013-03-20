6b08ecb7c5fe5d826aa478262722207b82a79c64
Tatu Kairi <tatu.kairi@eficode.com>
2013-03-20 20:59:43 +0200

    moved necessary stuff to layout; made page to show inserted wishes

--- app.js
+++ app.js
@@ -9,13 +9,35 @@ app.set( "views", __dirname + "/public" );
 app.set( "view options", { "layout": false} );
 
 app.use(express.static( __dirname + '/public' ));
+app.use( express.bodyParser() );
 
 app.get( "/", function(req, res) {
   res.render( "index.jade" );
 });
 
-app.post( "/create", function( req, res){
-  res.send( 404 );
+app.post( "/create", function( req, res ){
+  if ( !req.body.thankyou ) {
+    res.send(400);
+    return;
+  }
+  
+  db.thanks.save({
+      "thankyou": req.body.thankyou
+    , "date": new Date()
+  }, function( err ){
+    if ( err ){
+      res.send(500);
+      return;
+    }
+
+    res.send(200);
+  });
+});
+
+app.get( "/view", function( req, res ){
+  db.thanks.find().sort({ date: 1 }, function( err, thanks ){
+    res.render( "wishes.jade", { "thanks": thanks } );
+  });
 });
 
 var port = process.env.PORT || 8000;
