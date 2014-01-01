85b32a8036a32a4f86d3d1651ad3e89ba2c6524a
Tatu Kairi <tatu.kairi@eficode.com>
2014-01-01 02:07:58 +0200

    wtf

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post( "/create", function( req, res ){
 
 app.get( "/view", function( req, res ){
   
-  if ( (new Date()).getYear() < 2014 ) {
+  if ( (new Date()).getYear() < 2013 ) {
     res.render( "thanks.jade", { "thanks": [{
         "thankyou": "<h1>Ei vielä! :)</h1>"
       , "date": "Vasta " + moment().endOf( "year" ).fromNow()
